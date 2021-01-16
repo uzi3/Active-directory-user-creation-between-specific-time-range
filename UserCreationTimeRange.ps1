c2ef398b7f459d64fb586d14775e96ee16bc880e
uzi3 <43525327+uzi3@users.noreply.github.com>
2021-01-16 10:01:30 +0530

    first commit

---
+++ UserCreationTimeRange.ps1
@@ -0,0 +1,4 @@
+﻿$domain= Get-ChildItem env:userdomain | select -ExpandProperty value
+$start = Get-Date -Date 01-07-2017
+$end = Get-Date -Date 30-09-2017 -Hour 23 -Minute 59 -Second 59
+Get-ADUser -Filter {created -ge $start -and created -le $end} -Properties * | select samaccountname, created, userprincipalname | Export-Csv D:\$domain.csv -Append -NoTypeInformation
